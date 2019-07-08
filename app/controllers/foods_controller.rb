6f1557293dfe9b0b73d1baa7d83160c824dce44a
lpile <logan.pile@gmail.com>
2019-07-08 10:32:13 -0600

    Update #index action to recieve api

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,21 @@
 class FoodsController < ApplicationController
   def index
-  
+    conn = Faraday.new(:url => 'https://api.nal.usda.gov') do |faraday|
+      faraday.params['api_key'] = ENV['api_key']
+      faraday.params['q'] = params['q']
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+
+    response = conn.get('/ndb/search/')
+
+    results = JSON.parse(response.body, symbolize_names: true)[:list][:item]
+    search = JSON.parse(response.body, symbolize_names: true)[:list]
+
+    @food_count = search[:total]
+    @foods_search_name = params[:q]
+
+    @foods = results[0..9].map do |result|
+      Food.new(result)
+    end
   end
 end
